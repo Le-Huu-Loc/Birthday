4b1e47d3207c2852d10ed1e207c1173e765acede
Loc <huuloc271199@gmail.com>
2022-01-04 22:11:52 +0700

    first commit

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Làm người yêu tớ nhé I Love You",
+  titleHeader: "Happy Birthday Thảo Duyên",
   descriptionHeader:
-    "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
-  buttonYes: "Đồng Ý",
+    "Trước tiên anh muốn hỏi là em có yêu anh không?",
+  buttonYes: "Em Có",
   buttonNo: "Không! Còn lâu nhé.",
-  titleModar: "Cảm ơn em vì đồng ý",
+  titleModar: "Cảm ơn em vì đã yêu anh",
   descriptionModar:
-    'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+    'Em đã trưởng thành rồi đó 🥰. 18 tuổi, độ tuổi đẹp nhất đời người con gái! Trước hết anh chúc em hoàn thành kì thi này thật tốt, để cho em có 1 cái tuổi 18 không hối hận... Và kể từ hôm nay thì em cũng đã trưởng thành rồi 🤪, em đã có thể tự quyết định tương lai của mình, tuy nhiên anh chỉ muốn em mãi là 1 cô bé dễ thương, hồn nhiên yêu anh mà không cần toan tính ❤. Vì cái thế giới dành cho người trưởng thành thì chằng dễ dàng gì, có rất nhiều cám dỗ mà chỉ cần chọn sai thì em sẽ không có cơ hội để chọn lại nha em 😩. Cuối cùng anh chúc em luôn dễ thương, xinh đẹp, chúc cho những dự định trong tương lai của em sẽ thành công em nhé. À mà bớt làm anh cọc nữa nha vợ nha 😒 YÊU EM - 0501 ♥',
 };
